5e770955bc6b809f15eb05f369cdb5743739e6a5
JESUS SUBIAS <jesus.subias@unizar.es>
2026-07-12 17:27:05 +0200

    Add Beatitudes meeting proposal

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260712-reunion-samaritana";
+const DATA_VERSION = "20260712-reunion-bienaventuranzas";
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
 let toastTimer = null;
