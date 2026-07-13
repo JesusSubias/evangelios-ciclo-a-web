caee401ecc4e349f317238dc5479a8d7dc8daf95
JESUS SUBIAS <jesus.subias@unizar.es>
2026-07-13 18:37:37 +0200

    Añadir reunión Soy tu hijo

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260713-reunion-en-medio";
+const DATA_VERSION = "20260713-reunion-soy-tu-hijo";
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
 let toastTimer = null;
