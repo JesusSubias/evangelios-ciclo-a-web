4154fb72ed27fb4f38519885a68ed706ebf411f6
JesusSubias <130402250+JesusSubias@users.noreply.github.com>
2026-07-13 22:29:58 +0200

    Añadir reunión Dame tus ojos (#13)

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260713-reunion-sopla-en-mi";
+const DATA_VERSION = "20260713-reunion-dame-tus-ojos";
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
 let toastTimer = null;
