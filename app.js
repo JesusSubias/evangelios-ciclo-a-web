11b2e1440025e8782e8abe948884c8a495e9d6b4
JesusSubias <130402250+JesusSubias@users.noreply.github.com>
2026-07-15 00:26:34 +0200

    Add Camino meeting
    Publica la propuesta Camino, Verdad y Vida y sus imágenes hiperrealistas.

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260714-reunion-hosanna";
+const DATA_VERSION = "20260714-reunion-camino";
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
 let toastTimer = null;
