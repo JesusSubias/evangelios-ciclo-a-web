ab90661cbf5284ea193cd7908055860737992bfd
JESUS SUBIAS <jesus.subias@unizar.es>
2026-07-14 18:01:41 +0200

    Add Tú eres la puerta meeting

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260714-reunion-se-encontraron-la-vida";
+const DATA_VERSION = "20260714-reunion-tu-eres-la-puerta";
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
 let toastTimer = null;
