33b27e901d0055f2a48feab8924b62f5f9cd5305
JESUS SUBIAS <jesus.subias@unizar.es>
2026-07-15 18:02:50 +0200

    Add La noche no es dueña meeting

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260715-reunion-el-cielo-llamo-a-tu-puerta";
+const DATA_VERSION = "20260715-reunion-la-noche-no-es-duena";
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
 let toastTimer = null;
