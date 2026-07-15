06c506f57819e686243c3947b95abf2e0699d2e4
JesusSubias <130402250+JesusSubias@users.noreply.github.com>
2026-07-15 07:22:23 +0200

    Add Id meeting (#24)

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260715-reunion-cuida-la-vida";
+const DATA_VERSION = "20260715-reunion-id";
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
 let toastTimer = null;
