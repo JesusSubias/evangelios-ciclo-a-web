8cb5cbc8e2c19a0e309f5a53fee21528268eef93
JESUS SUBIAS <jesus.subias@unizar.es>
2026-07-16 00:07:15 +0200

    Publica la reunión Tres tiendas

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260715-reunion-un-vaso-de-agua";
+const DATA_VERSION = "20260715-reunion-tres-tiendas";
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
 let toastTimer = null;
