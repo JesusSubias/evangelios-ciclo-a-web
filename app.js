25ceeafd8ab7429d2e687fc6e44c89fcefcd7d63
JESUS SUBIAS <jesus.subias@unizar.es>
2026-07-19 13:51:28 +0200

    Homogeneiza las presentaciones de reunión

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260718-todo-hablaba-de-ti-ppt-legible";
+const DATA_VERSION = "20260719-ppt-homogeneizados";
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
 let toastTimer = null;
