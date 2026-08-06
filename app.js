4f0a03949bcdff33edc05e42e0f8c2b1c15b5201
JESUS SUBIAS <jesus.subias@unizar.es>
2026-08-06 07:35:47 +0200

    Corrige la canción al girar tabletas

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260805-editorial-art-v7";
+const DATA_VERSION = "20260806-editorial-art-v8";
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
 let toastTimer = null;
