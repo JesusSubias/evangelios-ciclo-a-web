f34f07f50b87bfc55ba35ca892645979fd4d169b
JESUS SUBIAS <jesus.subias@unizar.es>
2026-08-17 06:13:25 +0200

    Publicar Quién soy contigo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260808-creere-por-ti-v14";
+const APP_VERSION = "20260817-quien-soy-contigo-v15";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
