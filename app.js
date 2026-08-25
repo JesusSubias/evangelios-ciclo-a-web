322f0b2fd1bdf72cfc46bd40d8a8d49d452b673a
JESUS SUBIAS <jesus.subias@unizar.es>
2026-08-25 23:51:39 +0200

    Actualizar producción de Un vaso de agua

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260825-dame-tus-ojos-v19";
+const DATA_VERSION = "20260825-un-vaso-de-agua-v20";
 const OFFLINE_MESSAGE_TIMEOUT = 45000;
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260825-dame-tus-ojos-v19";
+const APP_VERSION = "20260825-un-vaso-de-agua-v20";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
