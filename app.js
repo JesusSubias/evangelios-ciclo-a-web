68fdf3ab85a014119b9583897edd7efc01a9e003
JESUS SUBIAS <jesus.subias@unizar.es>
2026-08-26 14:04:48 +0200

    Actualizar producción de Todo hablaba de ti

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260825-un-vaso-de-agua-v20";
+const DATA_VERSION = "20260826-todo-hablaba-de-ti-v21";
 const OFFLINE_MESSAGE_TIMEOUT = 45000;
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260825-un-vaso-de-agua-v20";
+const APP_VERSION = "20260826-todo-hablaba-de-ti-v21";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
