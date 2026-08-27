f18dcf253d0271f56fdfc0c5ce7528ba7a6b6cf9
JESUS SUBIAS <jesus.subias@unizar.es>
2026-08-27 18:02:45 +0200

    Actualizar producción de Id

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260827-sopla-en-mi-v29";
+const DATA_VERSION = "20260827-id-v30";
 const OFFLINE_MESSAGE_TIMEOUT = 45000;
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260827-sopla-en-mi-v29";
+const APP_VERSION = "20260827-id-v30";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
