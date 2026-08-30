e30599c5bcda2bc7dbcb9cdfede1079d1088b063
JESUS SUBIAS <jesus.subias@unizar.es>
2026-08-30 14:22:46 +0200

    Actualizar producción de Yo soy la Resurrección

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260830-hosanna-v36";
+const APP_VERSION = "20260830-yo-soy-la-resurreccion-v37";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
