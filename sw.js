d679ad98d329270a86c9a6b29db50cf44adb471f
JESUS SUBIAS <jesus.subias@unizar.es>
2026-08-31 00:26:44 +0200

    Actualizar producción de Enséñame a mirar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260830-yo-soy-la-resurreccion-v37";
+const APP_VERSION = "20260831-ensename-a-mirar-v38";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
