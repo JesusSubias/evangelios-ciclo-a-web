fef9349ed70997baa63adcbb30fc340b5274e723
JESUS SUBIAS <jesus.subias@unizar.es>
2026-08-30 10:50:20 +0200

    Actualizar producción de En medio

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260829-entre-tu-y-yo-v33";
+const DATA_VERSION = "20260830-en-medio-v34";
 const OFFLINE_MESSAGE_TIMEOUT = 45000;
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260829-entre-tu-y-yo-v33";
+const APP_VERSION = "20260830-en-medio-v34";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
