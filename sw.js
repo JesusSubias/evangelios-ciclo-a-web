3850b35f3f888283a93cd09acfdcb1795f0e49dd
JESUS SUBIAS <jesus.subias@unizar.es>
2026-08-31 20:38:58 +0200

    Actualizar producción de Contempla al Cordero

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260831-sigueme-v45";
+const APP_VERSION = "20260831-contempla-al-cordero-v46";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
