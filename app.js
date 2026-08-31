4e2f3b7b8e6077f244b0ccc676945a91b0b58d45
JESUS SUBIAS <jesus.subias@unizar.es>
2026-08-31 19:40:57 +0200

    Actualizar producción de Sígueme

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260831-el-reino-empieza-abajo-v44";
+const DATA_VERSION = "20260831-sigueme-v45";
 const OFFLINE_MESSAGE_TIMEOUT = 45000;
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260831-el-reino-empieza-abajo-v44";
+const APP_VERSION = "20260831-sigueme-v45";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
