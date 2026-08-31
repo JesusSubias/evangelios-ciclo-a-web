9e7aa087f5510d833cc5a30dfa59f9b70c83cbc6
JESUS SUBIAS <jesus.subias@unizar.es>
2026-08-31 13:03:11 +0200

    Actualizar producción de Tres tiendas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260831-dame-ese-agua-v39";
+const APP_VERSION = "20260831-tres-tiendas-v40";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
