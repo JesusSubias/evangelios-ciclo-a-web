64b9955539d6fa5c4f1392ce55ee57518f2db58b
JESUS SUBIAS <jesus.subias@unizar.es>
2026-09-01 12:55:37 +0200

    Actualizar producción de Despierta mi vida

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260901-va-sin-precio-v51";
+const APP_VERSION = "20260901-despierta-mi-vida-v52";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
