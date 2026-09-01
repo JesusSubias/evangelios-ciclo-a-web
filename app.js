071e06fb11afe03c2ea47b8d874fe36ce0fe4064
JESUS SUBIAS <jesus.subias@unizar.es>
2026-09-01 12:14:54 +0200

    Actualizar producción de Va sin precio

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260901-en-medio-de-vosotros-v50";
+const DATA_VERSION = "20260901-va-sin-precio-v51";
 const OFFLINE_MESSAGE_TIMEOUT = 45000;
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260901-en-medio-de-vosotros-v50";
+const APP_VERSION = "20260901-va-sin-precio-v51";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
