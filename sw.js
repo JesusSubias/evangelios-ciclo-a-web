5710605e85f989678ceb83c3a6e48cc922d8cd57
JESUS SUBIAS <jesus.subias@unizar.es>
2026-09-01 13:07:57 +0200

    Corregir letra de El cielo llamó a tu puerta

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260901-despierta-mi-vida-v52";
+const APP_VERSION = "20260901-el-cielo-letra-v53";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
