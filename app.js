de7c7319cbdb8cab7b6c1623334c5d4cdca48a72
JESUS SUBIAS <jesus.subias@unizar.es>
2026-09-01 16:46:19 +0200

    Aclarar los criterios de participación

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ const seasonColors = {
 };
 
 const app = document.querySelector("#app");
-const DATA_VERSION = "20260901-el-cielo-letra-v53";
+const DATA_VERSION = "20260901-criterios-claros-v54";
 const OFFLINE_MESSAGE_TIMEOUT = 45000;
 const calendarWeekdays = ["L", "M", "X", "J", "V", "S", "D"];
 const monthFormatter = new Intl.DateTimeFormat("es-ES", { month: "long", year: "numeric" });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260901-el-cielo-letra-v53";
+const APP_VERSION = "20260901-criterios-claros-v54";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
