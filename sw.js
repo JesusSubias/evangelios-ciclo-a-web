b05c1887de8d55e9fe803a920a54bb5b71ecd13c
JESUS SUBIAS <jesus.subias@unizar.es>
2026-09-15 17:09:58 +0200

    Publicar la última corrección del audio del 20 de septiembre

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260915-audio-todo-este-tiempo-contigo-v67";
+const APP_VERSION = "20260915-audio-todo-este-tiempo-contigo-v68";
 const CACHE_PREFIX = "evgl-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${APP_VERSION}`;
